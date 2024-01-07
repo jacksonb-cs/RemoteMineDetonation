824ddba93c041f807e2b1d4ad038fb3a0e0052bb
Jackson Bullard <jacksonbullard@gmail.com>
2024-01-07 13:45:06 -0600

    Add initial plugin code

---
+++ RemoteMineDetonation.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace RemoteMineDetonation
+{
+	[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public const string PLUGIN_GUID = "com.jacksonb-cs.RemoteMineDetonation";
+		public const string PLUGIN_NAME = "RemoteMineDetonation";
+		public const string PLUGIN_VERSION = "1.0.0";
+
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+		}
+	}
+}
